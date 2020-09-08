a0b68b410e747e1824f81ac901e8098c8c372b1a
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-09-08 14:16:44 -0500

    Starting codepipelines

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from pipelines_webinar.pipelines_webinar_stack import PipelinesWebinarStack
-
+from pipelines_webinar.pipeline_stack import PipelineStack
 
 app = core.App()
 PipelinesWebinarStack(app, "pipelines-webinar")
-
+PipelineStack(app,'PipelineStack', env = {
+    'region':'eu-central-1'
+})
 app.synth()
