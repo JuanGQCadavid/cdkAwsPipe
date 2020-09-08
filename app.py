d2580644cda1511b77d8a37f4a3b61445aa1d9f0
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-09-08 15:50:02 -0500

    Adding pre-prod stage

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
-from pipelines_webinar.pipelines_webinar_stack import PipelinesWebinarStack
 from pipelines_webinar.pipeline_stack import PipelineStack
 
 app = core.App()
-PipelinesWebinarStack(app, "pipelines-webinar")
 PipelineStack(app,'PipelineStack', env = {
     'region':'eu-central-1'
 })
--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import core
 from aws_cdk import aws_codepipeline as codepipeline
 from aws_cdk import aws_codepipeline_actions as cpactions
 from aws_cdk import pipelines 
+from .webservice_stage import WebServiceStage
 
 class PipelineStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs):
@@ -10,7 +11,7 @@ class PipelineStack(core.Stack):
         source_artifact = codepipeline.Artifact()
         cloud_assembly_artifact = codepipeline.Artifact()
 
-        pipelines.CdkPipeline(self, 'PipelineToy',
+        pipeline = pipelines.CdkPipeline(self, 'PipelineToy',
             cloud_assembly_artifact=cloud_assembly_artifact,
             pipeline_name='WebinarPipeline',
             source_action= cpactions.GitHubSourceAction(
@@ -26,3 +27,7 @@ class PipelineStack(core.Stack):
                 install_command='npm install -g aws-cdk && pip install -r requirements.txt',
                 synth_command='cdk synth')
             )
+
+        pipeline.add_application_stage(WebServiceStage(self,'Pre-Prod', env={
+            'region':'eu-central-1'
+        }))
